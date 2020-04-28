6ba56b9ce8ca9ef432a712982ffb150b2e3e3403
mirimhee <48482259+LIMMIHEE@users.noreply.github.com>
2020-04-28 12:20:32 +0900

    버튼 메세지 이벤트 추가

--- HelloWPF/MainWindow.xaml.cs
+++ HelloWPF/MainWindow.xaml.cs
@@ -24,5 +24,11 @@ namespace HelloWPF
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("안녕~","반가운 인사",MessageBoxButton.YesNo
+                , MessageBoxImage.Question);
+        }
     }
 }
